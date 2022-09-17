5c232af56983a21567a063b4d3368e0b206f0a60
MICHAEL GLIENECKE <michael@glienecke.de>
2022-09-17 12:49:43 +0100

    orders.json generation for samples

--- IlpRestServer/src/main/java/uk/ac/ed/inf/ilpRestServer/IlpOrdersSampleDataGenerator.java
+++ IlpRestServer/src/main/java/uk/ac/ed/inf/ilpRestServer/IlpOrdersSampleDataGenerator.java
@@ -26,10 +26,8 @@ public class IlpOrdersSampleDataGenerator {
         var restaurants = new IlpRestService().restaurants();
 
 
-        // we use 2 years
+        // we use a smaller period than 2 years as the result gets huge
         var startDate = LocalDate.of(2023, 1, 1);
-        var endDate =  LocalDate.of(2023, 5, 31);
-
 
         // needed to create mock data
         MockNeat mock = MockNeat.threadLocal();
@@ -37,16 +35,15 @@ public class IlpOrdersSampleDataGenerator {
         // where our result will be returned
         List<Order> orderList = new ArrayList<>();
 
-        // as long as we are before our end data
+        // iterate over each date in the range
         var currentDate = startDate;
-        while (currentDate.isBefore(endDate)) {
+        while (currentDate.isBefore(LocalDate.of(2023, 5, 31))) {
 
+            // traverse over all possible order outcomes to generate one record for each outcome possible
             for (var outcome : OrderOutcome.values()){
 
+                // create a valid order with default values
                 var order = new OrderWithOutcome();
-
-
-                // some default values
                 order.orderNo = String.format("%08X", ThreadLocalRandom.current().nextInt(1, Integer.MAX_VALUE));
                 order.orderDate = String.format("%04d-%02d-%02d", currentDate.getYear(), currentDate.getMonthValue(), currentDate.getDayOfMonth());
                 order.cvv = mock.cvvs().get();
@@ -54,20 +51,22 @@ public class IlpOrdersSampleDataGenerator {
                 order.customer = mock.names().get();
                 order.creditCardExpiry = String.format("%02d/%02d", ThreadLocalRandom.current().nextInt(1, 12), ThreadLocalRandom.current().nextInt(24, 28));
 
-                // every order is valid by default
+                // every order has the defined outcome
                 order.orderOutcome = outcome;
 
-
-
-                // get the current restaurant
+                // get a random restaurant
                 var currentRestaurant = restaurants[ThreadLocalRandom.current().nextInt(0, restaurants.length-1)];
 
+                // and load the order items plus the price
                 order.orderItems = Arrays.stream(currentRestaurant.menu).map(m -> m.name).toArray(String[]::new);
                 order.priceTotalInPence = Arrays.stream(currentRestaurant.menu).map(m -> m.priceInPence).reduce(0, Integer::sum) + Order.OrderChargeInPence;
 
+                // now modify the wrong entries according to the enum
                 switch (outcome){
                     case InvalidCvv -> {
                         var len = Integer.toString(ThreadLocalRandom.current().nextInt(1, 8));
+
+                        // 3 digit CVV would be valid
                         if (len.equals("3")){
                             len = "4";
                         }
@@ -81,14 +80,21 @@ public class IlpOrdersSampleDataGenerator {
                         order.creditCardNumber = String.format(formatString, ThreadLocalRandom.current().nextLong(1, 9999999999999999L));
                     }
                     case InvalidTotal -> {
-                        order.priceTotalInPence +=  ThreadLocalRandom.current().nextInt(-100, 1000);
+                        // by adding something to the order this invalidates the total -> just make sure it is not 0 (which would be correct again)
+                        var add = ThreadLocalRandom.current().nextInt(-100, 1000);
+                        if (add == 0){
+                            add = 1;
+                        }
+                        order.priceTotalInPence += add;
                     }
 
                     case InvalidExpiryDate -> {
+                        // expiry date is always in the past
                         order.creditCardExpiry = String.format("%02d/%02d", ThreadLocalRandom.current().nextInt(1, 20), ThreadLocalRandom.current().nextInt(2, 19));
                     }
 
                     case InvalidPizzaNotDefined -> {
+                        // add a pizza which does not exist
                         ArrayList<String> items = new ArrayList<>();
                         items.addAll(Arrays.stream(order.orderItems).toList());
                         items.add("Pizza-Surprise " + new Random(10000).nextInt());
@@ -96,6 +102,7 @@ public class IlpOrdersSampleDataGenerator {
                     }
 
                     case InvalidPizzaCount -> {
+                        // just multiply the ordered pizzas (5 is always more than the valid maximum)
                         ArrayList<String> items = new ArrayList<>();
                         items.addAll(Arrays.stream(order.orderItems).toList());
                         items.addAll(Arrays.stream(order.orderItems).toList());
@@ -109,6 +116,7 @@ public class IlpOrdersSampleDataGenerator {
                     }
 
                     case InvalidPizzaCombinationMultipleSuppliers -> {
+                        // mix pizzas from a different supplier
                         currentRestaurant = restaurants[ThreadLocalRandom.current().nextInt(0, restaurants.length-1)];
 
                         ArrayList<String> items = new ArrayList<>();
@@ -119,6 +127,7 @@ public class IlpOrdersSampleDataGenerator {
                     }
 
                     case Invalid -> {
+                        // this code is not used as such for results - only for initialization
                         continue;
                     }
                 }
@@ -129,6 +138,7 @@ public class IlpOrdersSampleDataGenerator {
             // find another date
             currentDate = currentDate.plusDays(1);
         }
+
 
         var writer = new BufferedWriter(new FileWriter("orders.json"));
         writer.write(new Gson().toJson(orderList.toArray()));
